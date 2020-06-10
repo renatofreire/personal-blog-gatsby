72f8c19335c8aae1e8db1a7d68db28857cd0f305
renato <contato.renatofreire@gmail.com>
2020-06-10 19:42:56 -0300

    Create global styles
    - create CSS reset
    - create Container component

---
+++ src/styles/container.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 10%;
+
+  @media (min-width: 1080px) {
+    width: 100%;
+    max-width: 980px;
+    margin: 0 auto;
+  }
+`
+export default Container
