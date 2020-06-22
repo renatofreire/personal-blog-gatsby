832a2695fa9ae0a69870b680438ac83fbac66926
renato <contato.renatofreire@gmail.com>
2020-06-22 10:27:12 -0300

    Make Title component more generic
    - add heading prop to decide which tag will be used

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import colors from "../../styles/colors"
 import fonts from "../../styles/fonts"
 
-const StyledTitle = styled.h1`
+const defaultStyle = css`
   font: ${fonts.title};
   color: ${colors.dark};
   font-weight: bold;
@@ -17,9 +17,39 @@ const StyledTitle = styled.h1`
   margin-bottom: 0.5em;
 `
 
-const Title = ({ children }) => <StyledTitle>{children}</StyledTitle>
+const Heading1 = styled.h1`
+  ${defaultStyle}
+`
+const Heading2 = styled.h2`
+  ${defaultStyle}
+`
+const Heading3 = styled.h3`
+  ${defaultStyle}
+`
+const Heading4 = styled.h4`
+  ${defaultStyle}
+`
+const Heading5 = styled.h5`
+  ${defaultStyle}
+`
+const Heading6 = styled.h6`
+  ${defaultStyle}
+`
+const headings = [Heading1, Heading2, Heading3, Heading4, Heading5, Heading6]
+
+const Title = ({ className, heading, children }) => {
+  const Heading = headings[heading - 1] || Heading1
+  return <Heading className={className}>{children}</Heading>
+}
+
+Title.defaultProps = {
+  className: "",
+  heading: 1,
+}
 
 Title.propTypes = {
+  className: PropTypes.string,
+  heading: PropTypes.number,
   children: PropTypes.node.isRequired,
 }
 
