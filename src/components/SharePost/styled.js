575eeb92d457a769af1aa5b8f99ff66b95e4e431
renato <contato.renatofreire@gmail.com>
2020-09-11 22:09:14 -0300

    Improve CSS layout in share component

--- src/components/SharePost/styled.js
+++ src/components/SharePost/styled.js
@@ -3,16 +3,18 @@ import styled from "styled-components"
 import { colors, fonts, radius, shadows } from "../../styles/tokens"
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 1em;
   margin: 2em 0;
   background-color: ${colors.backgrounds.level2};
   border-radius: ${radius.small};
   box-shadow: ${shadows.default};
 
-  @media (min-width: 430px) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  @media (max-width: 430px) {
+    flex-direction: column;
+    align-items: flex-start;
   }
 `
 export const Text = styled.p`
@@ -24,6 +26,11 @@ export const Text = styled.p`
 export const LinksContainer = styled.div`
   display: flex;
   align-items: center;
+
+  @media (max-width: 430px) {
+    margin-top: 1em;
+    align-self: flex-end;
+  }
 `
 
 export const ShareLink = styled.a`
