2a2c30089627859c434689bc3c15835b47127e27
Renato Freire <contato.renatofreire@gmail.com>
2022-07-23 23:03:29 -0300

    Create structure to dark/light mode

---
+++ src/components/ThemeButton/index.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+
+import * as S from "./styled"
+
+const ThemeButton = () => {
+  const [theme, setTheme] = useState(null);
+  
+  useEffect(() => {
+    setTheme(window.__theme);
+    window.__onThemeChange = setTheme;
+  }, []);
+  
+  const isDarkMode = theme === "dark";
+
+  function defineTheme() {
+    window.__setPreferredTheme(isDarkMode ? 'light' : 'dark');
+  }
+
+  
+  
+  return (
+    <S.Switch
+      title={`Alterar para tema ${isDarkMode ? 'claro' : 'escuro'}`}
+    >
+      <S.SwitchInput 
+        type="checkbox" 
+        onChange={defineTheme}
+      />
+      <S.Slider checked={isDarkMode}></S.Slider>
+    </S.Switch>
+  );
+}
+
+export default ThemeButton;
